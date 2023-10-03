498983a922bbfd155f85e696049f55579ca83a0e
harsh <harshsngh2004@gmail.com>
2023-10-03 11:35:00 +0530

    Adding some comments

--- AREA OF SHAPES/areaOfCirlce.cpp
+++ AREA OF SHAPES/areaOfCirlce.cpp
@@ -5,7 +5,7 @@ int main ()
      float Radius;
      cout<<"Enter the Radius of the Circle : ";
      cin>>Radius;
-
-     cout<<"Area of circle is : "<<3.1415 * Radius * Radius;
+//area of the circle..
+  cout<<"Area of circle is : "<<3.1415 * Radius * Radius;
     return 0;
 }
