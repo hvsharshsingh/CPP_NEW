512e3edf41e29776b531d15a935caa74549e8cbe
Harsh Vardhan Singh <145780320+hvsharshsingh@users.noreply.github.com>
2023-10-09 11:55:44 +0530

    Update tableOf19.cpp

--- LOOPS/tableOf19.cpp
+++ LOOPS/tableOf19.cpp
@@ -1,10 +1,13 @@
 # include <iostream>
 using namespace std;
-int main ()
-{
-    for (int i = 19; i <= 190; i = i + 19)
+int main (){
+int n;
+cout<<"Enter a number : ";
+cin>>n;
+
+    for (int i = 1; i <= 10; i++)
     {
-    cout<<i<<" "<<endl;
+    cout<< n * i <<" "<<endl;
     }
     return 0;
 }
